006d043793704fdf3b81169ce183f4fb13d06c2f
adam <adam@Adams-MacBook-Pro.local>
2019-04-04 22:00:53 -0700

    Changed result count, added nav functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,47 +7,72 @@ export default class SearchBar extends Component {
     url: "https://api.giphy.com/v1/gifs/search?api_key=",
     key: "iNACUQW9yFvcDyaPwxNVy45mKeNNx9XP&",
     queryText: "",
-    resultsPerPage: 100,
+    pageOffset: 0,
+    resultsTotal: "",
     gifs: {},
-    pagination: [],
-    listStyle: "smallThumbs"
+    listStyle: "smallThumbs",
+    pageNumber: "1"
   };
 
   onChange = e => {
     this.setState({ queryText: e.target.value });
   };
 
-  checkKey = e => {
-    if (e.key === "Enter") {
-      this.search();
-    }
-  };
-
   search = () => {
     if (this.state.queryText) {
-      const { url, key, queryText, resultsPerPage } = this.state;
+      const { url, key, queryText, pageOffset } = this.state;
       axios
         .get(
-          `${url}${key}&q=${queryText}&limit=${resultsPerPage}&offset=0&rating=R&lang=en`
+          `${url}${key}&q=${queryText}&limit=0&offset=${pageOffset}&rating=R&lang=en`
         )
         .then(response => {
           this.setState({
-            gifs: response.data.data
-            // pagination: response.data.pagination
+            gifs: response.data.data,
+            resultsTotal: response.data.pagination.total_count
           });
-          console.log(this.state.pagination);
+
+          if (
+            this.state.pageOffset === 0
+              ? this.setState({ pageOffset: 25 })
+              : null
+          )
+            console.log(this.state.resultsTotal);
         })
         .catch(err => console.log(err));
 
       this.setState.queryText = "";
     } else {
-      alert("No empty searches ノ( º _ ºノ) ");
+      alert("You didn't type anything? ノ( º – ºノ) ");
       return;
     }
   };
 
   setListStyle = e => {
     this.setState({ listStyle: e.target.name });
+  };
+  // update
+  nextPage = () => {
+    this.setState({
+      pageOffset: this.state.pageOffset + 25,
+      pageNumber: this.state.pageNumber + 1
+    });
+    this.search();
+  };
+  previousPage = () => {
+    if (this.state.pageNumber > 1) {
+      this.setState({
+        pageOffset: this.state.pageOffset - 25,
+        pageNumber: this.state.pageNumber - 1
+      });
+      this.search();
+    }
+  };
+
+  // run search if user hits enter key
+  checkKey = e => {
+    if (e.key === "Enter") {
+      this.search();
+    }
   };
 
   render() {
@@ -85,10 +110,29 @@ export default class SearchBar extends Component {
               onClick={this.setListStyle}
             />
           </div>
-        </div>
+          <span className="display-count">
+            {this.state.resultsTotal > 0
+              ? `Showing  ${this.state.pageOffset}
+                out of ${this.state.resultsTotal}`
+              : null}
+          </span>
 
+          <div className={"nav-button-container"}>
+            <button className={"nav-buttons"} onClick={this.previousPage}>
+              previous page
+            </button>
+
+            <button className={"nav-buttons"} onClick={this.nextPage}>
+              next page
+            </button>
+          </div>
+        </div>
         {this.state.gifs.length > 0 ? (
-          <Gallery gifs={this.state.gifs} listStyle={this.state.listStyle} />
+          <Gallery
+            gifs={this.state.gifs}
+            listStyle={this.state.listStyle}
+            pageOffset={this.state.pageOffset}
+          />
         ) : null}
       </div>
     );
